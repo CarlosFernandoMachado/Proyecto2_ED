6d2aaaf7059c780219899d71b3100151ef8cd976
CarlosFernandoMachado <testsubjectno2@hotmail.com>
2016-08-21 17:20:21 -0600

    quinto commit
    ya lee el archivo

--- Proyecto_2/src/proyecto_2/Proyecto_2.java
+++ Proyecto_2/src/proyecto_2/Proyecto_2.java
@@ -5,6 +5,9 @@
  */
 package proyecto_2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  *
  * @author carlos
@@ -16,6 +19,31 @@ public class Proyecto_2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        VSAList herarquia = leerarchivo();
+        for (int i = 0; i < herarquia.Size(); i++) {
+            System.out.println(((String)(herarquia.get(i))));
+        }
+        if (((String)(herarquia.get(0))).charAt(0) == '\t') {
+            System.out.println("SI FUNCIONA");
+        }
     }
-    
+    public static VSAList leerarchivo() {
+        VSAList herarquia = new VSAList();
+        String cadena;
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+            fr = new FileReader("Arbol.txt");
+            br = new BufferedReader(fr);
+            while ((cadena = br.readLine()) != null) {
+                herarquia.add(cadena);
+            }
+        } catch (Exception e) {
+        }
+        try {
+            br.close();
+        } catch (Exception e) {
+        }
+        return herarquia;
+    }
 }
